cc40187189c27110ed406fe54337d6066a8189f3
Shubham Sharma <69618795+spookysaint@users.noreply.github.com>
2022-05-06 21:53:34 +0530

    Update and rename app.py to main.py

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ def videoplayer():
     return render_template('videoplayer.html', url=request.args.get('url'));
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
