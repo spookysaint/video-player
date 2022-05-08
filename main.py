5140a97437d233cc613a46ea8aede6efa58de74b
Shubham Sharma <69618795+spookysaint@users.noreply.github.com>
2022-05-08 20:20:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,5 +10,11 @@ def videoplayer():
     if not request.args.get('url'): return redirect('/')
     return render_template('videoplayer.html', url=request.args.get('url'));
 
+@app.route('/redditvideo/')
+def redditplayer():
+    if not request.args.get('url'): return redirect('/')
+    return render_template('hls.html', hls=request.args.get('url'));
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
